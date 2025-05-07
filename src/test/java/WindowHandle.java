334f56ddbb4570e9ab4d9fe45d7f85c2fb64f45d
rajeevforindia <rajeevr.qa@gmail.com>
2025-05-07 09:32:30 +0530

    First commit

---
+++ src/test/java/WindowHandle.java
@@ -0,0 +1,41 @@
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowHandle {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\eclipse-workspace1\\Important_in_Selenium\\useful_driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.globalsqa.com/demo-site/select-dropdown-menu/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//div[@class='subheader_inside']/a[5]")).click();
+		driver.findElement(By.xpath("//div[@class='subheader_inside']/a[4]")).click();
+		
+		// parrent window
+		String parentWindow = driver.getWindowHandle();
+		
+		// Store all window
+		
+		Set<String> allWindows = driver.getWindowHandles();
+		for(String window:allWindows) {
+			if(!window.equals(allWindows)) {
+
+				driver.switchTo().window(window);
+				System.out.println("Visited window title is" + driver.getTitle());
+				//break;
+			}
+			
+			//Return to parent window
+			
+			driver.switchTo().window(parentWindow);
+			driver.close();
+		}
+
+	}
+
+}
